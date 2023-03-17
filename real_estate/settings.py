55e0bed251aa669862571a82b3c1d1e6d019873c
erfan khaledi <99145548+erfankhaledi@users.noreply.github.com>
2023-03-17 07:22:33 -0700

    Update settings.py

--- real_estate/settings.py
+++ real_estate/settings.py
@@ -149,6 +149,6 @@ MESSAGE_TAGS = {
 # Emails
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'khaledierfan0@gmail.com'
-EMAIL_HOST_PASSWORD = 'juolqhocpyupngel'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS=True
